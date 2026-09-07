fbbb4b92db327a0da158f93672649cbaa4129fc7
MaxiSoriaGit <maximilianosoria42@gmail.com>
2026-09-07 10:25:02 -0300

    feat: crud de articulos con owner/admin y eliminacion logica

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
+import "./src/models/articleTag.model.js";
 import { startDB } from "./src/config/database.js";
 import { authRouter } from "./src/routes/auth.routes.js";
 import { userRouter } from "./src/routes/user.routes.js";
+import { tagRouter } from "./src/routes/tag.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api", authRouter);
 app.use("/api", userRouter);
+app.use("/api", tagRouter);
 
 
 app.listen(PORT, async () => {
